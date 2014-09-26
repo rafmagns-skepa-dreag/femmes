c3f031d3469c973dedbc05445b289abbbab8046f
Megan Renshaw <meganrenshaw19@gmail.com>
2014-09-26 14:41:12 -0500

    Adding initial files

---
+++ js/femmes.js
@@ -0,0 +1,11 @@
+$(function(){
+	SyntaxHighlighter.all();
+});
+$(window).load(function(){
+	$('.flexslider').flexslider({
+		animation: "slide",
+		start: function(slider){
+  			$('body').removeClass('loading');
+		}
+	});
+});
